abe4212c12ad8023b6c7a01e26e1f00a7c17f567
BAKA <allinonedibakar@gmail.com>
2018-02-16 01:26:56 +0530

    Added function to change position of pipes

--- NEAT Implementation/Pipe.py
+++ NEAT Implementation/Pipe.py
@@ -1,6 +1,7 @@
 import pygame
 import numpy as np
 
+# set the pipe normal or upside-down (use only 1 image of pipe to create two views)
 class PipeBlock(pygame.sprite.Sprite):
 
 	def __init__(self,image,upper):
@@ -15,7 +16,7 @@ class PipeBlock(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 
 
-
+# Pipe class
 class Pipe(pygame.sprite.Sprite):
 	
 	
@@ -23,21 +24,21 @@ class Pipe(pygame.sprite.Sprite):
 
 		pygame.sprite.Sprite.__init__(self)
 
-		self.screen = screen
-		self.lowerBlock = PipeBlock('../img/pipe.png',False)
-		self.upperBlock = PipeBlock('../img/pipe.png',True)
+		self.screen = screen # surface/screen
+		self.lowerBlock = PipeBlock('../img/pipe.png',False) # pipe image defualt
+		self.upperBlock = PipeBlock('../img/pipe.png',True) # pipe image rotated upside-down
 		self.gapBetweenPipes  = 100 # gaps between pipe
-		self.surfaceWidth = screen.get_width()
+		self.surfaceWidth = screen.get_width() # screen width
 
 		self.pipeWidth = self.upperBlock.rect.width
-		self.x = x
+		self.x = x # initial pipe position
 		
 
-		heights = self.getHeight()
+		heights = self.getHeight() # height
 		self.upperY, self.lowerY = heights[0], heights[1]
 
-		self.behindBird = 0
-		self.draw()
+		self.behindBird = 0 # by default pipe is ahead of bird
+		self.draw() # draw over screen
 
 
 	def getHeight(self):
@@ -70,3 +71,9 @@ class Pipe(pygame.sprite.Sprite):
 
 		self.draw()
 		return([self.x+(self.pipeWidth/2), self.upperY, self.lowerY])
+
+	# setting position of pipe
+	def setPos(self, x):
+		self.x = x
+		heights = self.getHeight() # height
+		self.upperY, self.lowerY = heights[0], heights[1]
